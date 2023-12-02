e5e6197d435961467ba755569a61b74cebf5ba82
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-12-02 10:22:25 -0300

    added controller to test

--- src/main/java/br/com/seteideias/springkeycloack/controller/Controller.java
+++ src/main/java/br/com/seteideias/springkeycloack/controller/Controller.java
@@ -1,2 +1,29 @@
-package br.com.seteideias.springkeycloack.controller;public class Controller {
+package br.com.seteideias.springkeycloack.controller;
+
+import br.com.seteideias.springkeycloack.service.CadastroProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class Controller {
+
+    @Autowired
+    private CadastroProdutoService service;
+
+    @GetMapping("/")
+    public List<String> productList(){
+        return Arrays.asList("produto1","produto2");
+    }
+
+    @PostMapping("/")
+    protected void create() {
+        service.doCadastro();
+    }
 }
---
+++ src/main/java/br/com/seteideias/springkeycloack/service/CadastroProdutoService.java
@@ -0,0 +1,12 @@
+package br.com.seteideias.springkeycloack.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroProdutoService {
+
+    public String doCadastro(){
+        return "produto cadastrado";
+    }
+
+}
