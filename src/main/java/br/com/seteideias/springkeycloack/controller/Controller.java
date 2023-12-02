4e38a89c6112e822adaf0a1a9d16ee4c975d5e8e
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-12-02 10:22:25 -0300

    added controller to test

--- src/main/java/br/com/seteideias/springkeycloack/controller/Controller.java
+++ src/main/java/br/com/seteideias/springkeycloack/controller/Controller.java
@@ -1,2 +1,13 @@
-package br.com.seteideias.springkeycloack.controller;public class Controller {
+package br.com.seteideias.springkeycloack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/get")
+    protected void get() {
+
+    }
 }
