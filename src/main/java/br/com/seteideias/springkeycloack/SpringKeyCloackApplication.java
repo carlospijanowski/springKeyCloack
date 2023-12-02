b5eb457654853b8c71b5a0b5b1ea0ed5c5b9671b
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-12-02 10:22:25 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/springkeycloack/SpringKeyCloackApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.springkeycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyCloackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeyCloackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/seteideias/springkeycloack/controller/Controller.java
@@ -0,0 +1,2 @@
+package br.com.seteideias.springkeycloack.controller;public class Controller {
+}
